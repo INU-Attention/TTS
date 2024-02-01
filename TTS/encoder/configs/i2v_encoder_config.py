473870f829d815439164fae0cadd4dc5f69627a9
MintyProtein <dnltjdwls1@naver.com>
2024-02-01 05:31:09 +0000

    UPDATE: i2v_encoder.py

---
+++ TTS/encoder/configs/i2v_encoder_config.py
@@ -0,0 +1,6 @@
+from dataclasses import asdict, dataclass, field
+from TTS.config.shared_configs import BaseAudioConfig, BaseDatasetConfig, BaseTrainingConfig
+from transformers.
+
+class I2VEncoderConfig(BaseTrainingConfig):
+    
--- TTS/encoder/models/i2v_encoder.py
+++ TTS/encoder/models/i2v_encoder.py
@@ -1,8 +1,14 @@
+import os
+import numpy as np
+import torch
 from torch import nn
+from TTS.encoder.configs.i2v_encoder_config import I2VEncoderConfig
+
 
 class I2VEncoder(nn.Module):
-    def __init__(self):
+    def __init__(self, config:I2VEncoderConfig):
         pass
-    def compute_embedding(self):
+    def compute_embedding(self, images: torch.Tensor)  -> torch.Tensor:
         pass
-    
+    def load_checkpoint(self, path: os.PathLike) -> None:
+        pass
