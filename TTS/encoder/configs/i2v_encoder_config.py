375a7a2a43bdfe885c6a9fa048ff32acee826863
MintyProtein <dnltjdwls1@naver.com>
2024-02-05 05:08:41 +0000

    UPDATE: i2v_encoder.py

--- TTS/encoder/configs/i2v_encoder_config.py
+++ TTS/encoder/configs/i2v_encoder_config.py
@@ -1,6 +1,31 @@
 from dataclasses import asdict, dataclass, field
-from TTS.config.shared_configs import BaseAudioConfig, BaseDatasetConfig, BaseTrainingConfig
-from transformers.
+from typing import Dict
+from transformers.models.clip.configuration_clip import CLIPVisionConfig
+from TTS.config.shared_configs import BaseTrainingConfig
 
+@dataclass
 class I2VEncoderConfig(BaseTrainingConfig):
+    model: str = "i2v_encoder"
+    class_name_key: str = "speaker_name"
+    model_params: Dict = field(
+        default_factory=lambda: {
+            "attention_dropout": 0.0,
+            "dropout": 0.0,
+            "hidden_act": "quick_gelu",
+            "hidden_size": 1024,
+            "image_size": 224,
+            "initializer_factor": 1.0,
+            "initializer_range": 0.02,
+            "intermediate_size": 4096,
+            "layer_norm_eps": 1e-05,
+            "model_type": "clip_vision_model",
+            "num_attention_heads": 16,
+            "num_channels": 3,
+            "num_hidden_layers": 24,
+            "patch_size": 14,
+            "projection_dim": 512,
+            "transformers_version": "4.35.2"
+        }
+    )
+    
     
--- TTS/encoder/models/i2v_encoder.py
+++ TTS/encoder/models/i2v_encoder.py
@@ -1,14 +1,34 @@
+"""
+https://github.com/huggingface/transformers/blob/v4.37.2/src/transformers/models/clip/modeling_clip.py
+"""
+
 import os
+from typing import Any, Optional, Tuple, Union
 import numpy as np
 import torch
 from torch import nn
 from TTS.encoder.configs.i2v_encoder_config import I2VEncoderConfig
-
+from transformers.models.clip.configuration_clip import CLIPVisionConfig
+from transformers.models.clip.modeling_clip import CLIPVisionTransformer
 
 class I2VEncoder(nn.Module):
-    def __init__(self, config:I2VEncoderConfig):
-        pass
+    def __init__(self, config:Optional[I2VEncoderConfig]=None):
+        super(I2VEncoder, self).__init__()
+        if config is None:
+            config = I2VEncoderConfig()
+        vision_config = CLIPVisionConfig(**config.model_params)
+        self.vision_model = CLIPVisionTransformer(vision_config)
+        self.projection = nn.Linear(
+            config.model_params["hidden_size"],
+            config.model_params["projection_dim"])
+        
+    def forward(self, images: torch.Tensor):
+        output = self.vision_model(images)[1]
+        output = self.projection(output)
+        return output
+
     def compute_embedding(self, images: torch.Tensor)  -> torch.Tensor:
-        pass
+        return self.forward(images)
+    
     def load_checkpoint(self, path: os.PathLike) -> None:
-        pass
+        self.load_state_dict(torch.load(path))
